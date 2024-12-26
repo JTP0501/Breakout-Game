34e3caa75eea102a334d26e36213d27b2d6ffd5b
JTP0501 <121999032+JTP0501@users.noreply.github.com>
2024-12-26 09:34:34 +0800

    added a skins attr for brick type 1 (regular bricks)

--- scripts/brick.py
+++ scripts/brick.py
@@ -1,37 +1,43 @@
 import pyxel
 
+from random import choice
+
 BrickType: dict[int, dict[str, int]] = {
     1: {
         "img": 0,
-        "u": 16,
+        "u": 0,
         "v": 0,
-        "w": 32,
-        "h": 8,
+        "w": 30,
+        "h": 14,
+        "colkey": 0,
         "score": 1
     },
     2: {
         "img": 0,
-        "u": 16,
-        "v": 8,
-        "w": 32,
-        "h": 8,
-        "score": 3
+        "u": 17,
+        "v": 17,
+        "w": 30,
+        "h": 14,
+        "colkey": 0,
+        "score": 1
     },
     3: {
         "img": 0,
-        "u": 16,
-        "v": 16,
-        "w": 32,
-        "h": 8,
-        "score": 5
+        "u": 17,
+        "v": 33,
+        "w": 30,
+        "h": 14,
+        "colkey": 0,
+        "score": 1
     },
     4: {
         "img": 0,
-        "u": 16,
-        "v": 24,
-        "w": 32,
-        "h": 8,
-        "score": 7
+        "u": 17,
+        "v": 1,
+        "w": 30,
+        "h": 14,
+        "colkey": 0,
+        "score": 1
     }, 
 }
 
@@ -41,24 +47,27 @@ class Brick:
         self.x = x
         self.y = y
         self.brick_type = brick_type
+        self.img = BrickType[self.brick_type]["img"]
         self.w = BrickType[brick_type]["w"]
         self.h = BrickType[brick_type]["h"]
         self.score = BrickType[brick_type]["score"]
+        self.skins_1 = [(17,1), (17,17), (17,33), (17,49), (17,65), (17,81)]
+        if brick_type == 1:
+            self.u, self.v = choice(self.skins_1)
+        else:
+            self.u = BrickType[brick_type]["u"]
+            self.v = BrickType[brick_type]["v"]
+        self.colkey = BrickType[brick_type]["colkey"]
     
     def draw(self) -> None:
         """ Drawing method for brick """
         pyxel.blt(
             self.x,
             self.y,
-            BrickType[self.brick_type]["img"],
-            BrickType[self.brick_type]["u"],
-            BrickType[self.brick_type]["v"],
-            BrickType[self.brick_type]["w"],
-            BrickType[self.brick_type]["h"]
+            self.img,
+            self.u,
+            self.v,
+            self.w,
+            self.h,
+            self.colkey
         )
-
-    def deflect_force(self, u: float) -> float:
-        """ Not used yet """
-        # Return the deflect force of the ball on the brick
-        force: float = (u - (self.x + self.w / 2)) / 10
-        return force
