1d710e5147f0cf185c397e046db8cd4f112e0420
JTP0501 <121999032+JTP0501@users.noreply.github.com>
2024-12-31 18:32:06 +0800

    modified initial speed and logic for collision to separate vs paddle  and vs bottom collision

--- scripts/reward.py
+++ scripts/reward.py
@@ -10,26 +10,29 @@ class Reward:
         self.w = 8
         self.h = 10
         self.accel = falling_accel
-        self.speed_y: float = 0.5 # initial speed
+        self.speed_y: float = pyxel.rndf(0.5,0.75) # initial speed
         self.P = points
 
 # +++++++++++++++++++++++++++++++++ HELPER METHODS +++++++++++++++++++++++++++++++++
     
-    def collides(self, paddle: Paddle) -> tuple[bool, int]:
-        """ Method that deals with all collisions of score objects with game elements """
+    def collides(self, paddle: Paddle) -> tuple[str | None, int]:
+        """
+        Method that deals with all collisions of score objects with game elements.
+        """
 
-        if self.y < pyxel.height: # checks if score object is still within the window
+        if self.y < pyxel.height:  # still within the screen
             if (
                 self.x < paddle.x + paddle.w and  # reward's left edge is to the left of paddle's right edge
-                self.x + self.w > paddle.x and             # reward's right edge is to the right of paddle's left edge
+                self.x + self.w > paddle.x and   # reward's right edge is to the right of paddle's left edge
                 self.y < paddle.y + paddle.h and # reward's top edge is above paddle's bottom edge
-                self.y + self.h > paddle.y                # reward's bottom edge is below paddle's top edge
-                ): # if the score object collides with the paddle
-                return True, self.P  # collision with paddle, points added
+                self.y + self.h > paddle.y       # reward's bottom edge is below paddle's top edge
+            ):
+                return "paddle", self.P  # collision with paddle
             else:
-                return False, 0 # didn't hit the bottom, but also didn't hit the paddle 
-        
-        return True, 0 # hit the bottom so don't add any to the score    
+                return None, 0  # no collision with paddle
+
+        return "bottom", 0  # hit the bottom of the screen
+
         
 
 # +++++++++++++++++++++++++++++++++ UPDATE METHODS +++++++++++++++++++++++++++++++++
